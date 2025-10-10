f5850645b8682edd385fa83169b2385f78e36b97
KendyReriani17 <peterkendyr@gmail.com>
2025-10-10 11:56:40 +0300

    Update Terraform configs

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,13 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
 # 1. VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = { Name = "ecommerce-vpc" }
 }
 
 # 2. Subnet
@@ -12,6 +15,35 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  tags = { Name = "ecommerce-subnet" }
+}
+# --- Internet Gateway ---
+resource "aws_internet_gateway" "ecommerce-igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "ecommerce-igw"
+  }
+}
+
+# --- Route Table ---
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ecommerce-igw.id
+  }
+
+  tags = {
+    Name = "ecommerce-public-rt"
+  }
+}
+
+# --- Route Table Association ---
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public-rt.id
 }
 
 # 3. Security Group
@@ -31,7 +63,7 @@ resource "aws_security_group" "allow_http_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -43,19 +75,18 @@ resource "aws_security_group" "allow_http_ssh" {
 }
 
 # 4. EC2 Instance
-resource "aws_instance" "app_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
 
-  subnet_id              = aws_subnet.public.id
+resource "aws_instance" "ecommerce_server" {
+  ami           = "ami-052064a798f08f0d3"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id   # <--- dynamic reference
+  key_name      = "ecommerce-key"
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
-  key_name               = var.key_name
 
-  tags = {
-    Name = "ecommerce-app-server"
-  }
+  tags = { Name = "ecommerce-app-server" }
 }
-
 output "instance_public_ip" {
-  value = aws_instance.app_server.public_ip
+  description = "Public IP of the e-commerce EC2 instance"
+  value       = aws_instance.ecommerce_server.public_ip
 }
+
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,4 +1,4 @@
-aws_region    = "us-west-2"
-ami_id        = "ami-01102c5e8ab69fb75" # Replace with latest Amazon Linux 2 AMI
+aws_region    = "us-east-1"
+ami_id        = "ami-052064a798f08f0d3" # Replace with latest Amazon Linux 2 AMI
 key_name      = "ecommerce-key" # Replace with your existing EC2 key pair
 my_ip         = "192.168.100.56/32" # Replace with your public IP (e.g. 196.###.###.###/32)
